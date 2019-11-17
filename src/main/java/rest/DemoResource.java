d400bc557e4fb0f6ece9c628bdebab4482671e34
fred8728 <fskn@hotmail.com>
2019-11-17 19:23:00 +0100

    myBackEnd

---
+++ src/main/java/rest/DemoResource.java
@@ -0,0 +1,171 @@
+package rest;
+
+import com.google.gson.Gson;
+import entities.User;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
+import javax.annotation.security.RolesAllowed;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import utils.EMF_Creator;
+
+/**
+ * @author lam@cphbusiness.dk
+ */
+@Path("info")
+public class DemoResource {
+
+    private static EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.DEV, EMF_Creator.Strategy.CREATE);
+
+    @Context
+    private UriInfo context;
+
+    @Context
+    SecurityContext securityContext;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getInfoForAll() {
+        return "{\"msg\":\"Hello anonymous\"}";
+    }
+
+    //Just to verify if the database is setup
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("all")
+    public String allUsers() {
+
+        EntityManager em = EMF.createEntityManager();
+        try {
+            List<User> users = em.createQuery("select user from User user").getResultList();
+            return "[" + users.size() + "]";
+        } finally {
+            em.close();
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("user")
+    @RolesAllowed("user")
+    public String getFromUser() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Welcome: " + thisuser + "\"}";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("admin")
+    @RolesAllowed("admin")
+    public String getFromAdmin() {
+        String thisuser = securityContext.getUserPrincipal().getName();
+        return "{\"msg\": \"Welcome (admin) " + thisuser + "\"}";
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("person/{id}")  
+    public String getStarwarz(@PathParam("id") int id) throws MalformedURLException, IOException{
+    URL url = new URL("https://swapi.co/api/people/"+id);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+    Scanner scan = new Scanner(con.getInputStream());
+    String jsonStr = null;
+    if (scan.hasNext()) {
+      jsonStr = scan.nextLine();
+      //jsonStr += "\n";
+    }
+    scan.close();
+    return jsonStr;
+  }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("planet/{id}")  
+    public String getPlanet(@PathParam("id") int id) throws MalformedURLException, IOException{
+    URL url = new URL("https://swapi.co/api/planets/"+id);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+    Scanner scan = new Scanner(con.getInputStream());
+    String jsonStr = null;
+    if (scan.hasNext()) {
+      jsonStr = scan.nextLine();
+      //jsonStr += "\n";
+    }
+    scan.close();
+    return jsonStr;
+  } 
+    
+      @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("vehicles/{id}")  
+    public String getSpecies(@PathParam("id") int id) throws MalformedURLException, IOException{
+    URL url = new URL("https://swapi.co/api/vehicles/"+id);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+    Scanner scan = new Scanner(con.getInputStream());
+    String jsonStr = null;
+    if (scan.hasNext()) {
+      jsonStr = scan.nextLine();
+      //jsonStr += "\n";
+    }
+    scan.close();
+    return jsonStr;
+  }
+    
+      @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("starships/{id}")  
+    public String getStarships(@PathParam("id") int id) throws MalformedURLException, IOException{
+    URL url = new URL("https://swapi.co/api/starships/"+id);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+    Scanner scan = new Scanner(con.getInputStream());
+    String jsonStr = null;
+    if (scan.hasNext()) {
+      jsonStr = scan.nextLine();
+      //jsonStr += "\n";
+    }
+    scan.close();
+    return jsonStr;
+  }
+    
+      @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("film/{id}")  
+    public String getFilms(@PathParam("id") int id) throws MalformedURLException, IOException{
+    URL url = new URL("https://swapi.co/api/films/"+id);
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+    con.setRequestProperty("User-Agent", "server"); //remember if you are using SWAPI
+    Scanner scan = new Scanner(con.getInputStream());
+    String jsonStr = null;
+    if (scan.hasNext()) {
+      jsonStr = scan.nextLine();
+      //jsonStr += "\n";
+    }
+    scan.close();
+    return jsonStr;
+  }
+
+}
